7952d215243f2f3ea0542e1aa33fc968e65abe81
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-07-15 09:24:31 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace dice_game
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var r = new Random();
+      var total = 0;
+      System.Console.WriteLine("Roll the Dice!");
+      System.Console.ReadKey();
+      var first = r.Next(1, 6);
+      var second = r.Next(1, 6);
+      for (int i = 0; i < 3; i++)
+      {
+        first = r.Next(1, 6);
+        second = r.Next(1, 6);
+
+        if (first == second)
+        {
+          System.Console.WriteLine($"you rolled double {first}, score: 0");
+          continue;
+        }
+        System.Console.WriteLine($"you Rolled a {first}, {second}, score: {first + second}");
+        total += first + second;
+      }
+      System.Console.WriteLine($"Total Score: {total}");
+    }
+  }
+}
